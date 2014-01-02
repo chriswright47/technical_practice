d66c7baa642d436ccd689bca35db8a00b8591b8c
Chris Wright <christopher.wright47@gmail.com>
2014-01-02 12:27:19 -0800

    added find function for tree prototype

--- trees/js_trees/trees.js
+++ trees/js_trees/trees.js
@@ -9,5 +9,31 @@ var Tree = function(root) {
 };
 
 Tree.prototype.find = function(value) {
-
+  return this.find_node(this.root, value);
 };
+
+Tree.prototype.find_node = function(node, value) {
+
+  if (!node || value === node.value) {
+    return node;
+  }
+  else if (value < node.value) {
+    return this.find_node(node.left, value);
+  }
+  else {
+    return this.find_node(node.right, value);
+  }
+};
+
+c = new Node(3, undefined, undefined);
+b = new Node(7, undefined, undefined);
+a = new Node(4, c, b);
+
+tree = new Tree(a);
+
+console.log(tree);
+console.log(tree.find(4) === a);
+console.log(tree.find(3) === c);
+console.log(tree.find(7) === b);
+console.log(tree.find(5) === undefined);
+console.log(tree.find(47) === undefined);
