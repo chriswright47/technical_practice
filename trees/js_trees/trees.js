9ce1ee838977407c1aaca4fa692fa5293a2cf89c
Chris Wright <christopher.wright47@gmail.com>
2014-01-02 12:15:01 -0800

    defined prototypes for node and tree

---
+++ trees/js_trees/trees.js
@@ -0,0 +1,13 @@
+var Node = function(value, left, right) {
+  this.value = value;
+  this.left = left;
+  this.right = right;
+};
+
+var Tree = function(root) {
+  this.root = root;
+};
+
+Tree.prototype.find = function(value) {
+
+};
