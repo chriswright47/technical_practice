3b23ba2b407d2f0ad618c62b90af7d817156d8b3
Chris Wright <christopher.wright47@gmail.com>
2014-01-02 13:38:57 -0800

    implemented find method for list

--- linked_lists/linked_list.rb
+++ linked_lists/linked_list.rb
@@ -5,6 +5,34 @@ Node = Struct.new(:value, :successor) do
 end
 
 List = Struct.new(:head) do
+  def self.build(args)
+    self.new(args[:head])
+  end
 
+  def find(value)
+    find_node(node: self.head, value: value)
+  end
+
+  def find_node(args)
+    node, value = args[:node], args[:value]
+    if !node || node.value == value
+      return node
+    else
+      args[:node] = node.successor
+      find_node(args)
+    end
+  end
 
 end
+
+c = Node.build(value: 47)
+b = Node.build(value: 23, successor: c)
+a = Node.build(value: 'original head', successor: b)
+
+list = List.build(head: a)
+
+puts list.head == a
+puts list.head.successor == b
+puts list.find('original head') == a
+puts list.find(47) == c
+puts list.find(46) == nil
