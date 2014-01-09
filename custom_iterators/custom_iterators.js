2986d244ff48c0285954ab40571ebe1e512a95e1
Chris Wright <christopher.wright47@gmail.com>
2014-01-09 14:48:15 -0800

    refactored a little

--- custom_iterators/custom_iterators.js
+++ custom_iterators/custom_iterators.js
@@ -1,9 +1,13 @@
+// each takes an array and a function to perform on each element
 var each = function(array, callback) {
   for (i=0; i < array.length; i++) {
     callback(array[i]);
   }
 }
 
+// map takes an array in as first argument (the original array),
+// followed by a function defining how to map elements from
+// the original array to new array
 var map = function(array, mappingFunction) {
   newArray = [];
   each(array, function(x) {
