a9a84b890d22fa0f993ac74ff3766819317b769c
Chris Wright <christopher.wright47@gmail.com>
2014-01-10 13:23:52 -0800

    added build and to_s methods for Card

--- cards/card.rb
+++ cards/card.rb
@@ -1,2 +1,16 @@
 # Instructions are to design data stuctures for a deck of cards
 # and explain how to subclass to implement certain card games
+
+Card = Struct.new(:name, :suit) do
+  def self.build(args)
+    self.new(args[:name], args[:suit])
+  end
+
+  def to_s
+    "#{name} of #{suit}"
+  end
+end
+
+king_h = Card.build(suit: 'hearts', name: 'king')
+puts king_h
+
