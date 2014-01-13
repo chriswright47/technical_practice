f52fe5a296856728aaa79f1417368e8ad876682a
Chris Wright <christopher.wright47@gmail.com>
2014-01-12 16:18:19 -0800

    ruby version now working for factorial zeros

--- practice_handout/factorial_zeros/factorial_zeros.rb
+++ practice_handout/factorial_zeros/factorial_zeros.rb
@@ -12,7 +12,25 @@ def factorial_zeros(n)
     two_count += count_twos(i)
     five_count += count_fives(i)
   end
-  return [two_count, five_count].min
+  [two_count, five_count].min
+end
+
+def count_twos(x)
+  count = 0
+  while x % 2 == 0
+    count += 1
+    x /= 2
+  end
+  count
+end
+
+def count_fives(x)
+  count = 0
+  while x % 5 == 0
+    count += 1
+    x /= 5
+  end
+  count
 end
 
 
