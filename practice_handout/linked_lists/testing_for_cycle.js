e7993c19555549b3050a045278f26b56d521f29e
Chris Wright <christopher.wright47@gmail.com>
2014-01-13 10:37:57 -0800

    added cyclic method to linked list prototype in js

---
+++ practice_handout/linked_lists/testing_for_cycle.js
@@ -0,0 +1,30 @@
+var Node = function(value, successor) {
+  this.value = value;
+  this.successor = successor;
+}
+
+var List = function(head) {
+  this.head = head;
+}
+
+List.prototype.cyclic = function() {
+  a = this.head;
+  b = this.head;
+  while (a.successor && b.successor) {
+    a = a.successor;
+    b = b.successor.successor;
+    if (a === b) {
+      return true;
+    }
+  }
+  return false;
+}
+
+c = new Node(47)
+b = new Node(23, c)
+a = new Node(1, b)
+c.successor = a
+
+list = new List(a)
+
+console.log(list.cyclic())
