0d7caed5c9d02de5c96481fe6cf6748d1f817423
Chris Wright <christopher.wright47@gmail.com>
2014-01-12 16:16:05 -0800

    added skeleton for factorial zeros method

--- practice_handout/factorial_zeros/factorial_zeros.rb
+++ practice_handout/factorial_zeros/factorial_zeros.rb
@@ -1,16 +1,21 @@
-def assert(recevied, expected)
+def assert(received, expected)
   if received == expected
     puts 'test passed'
   else
-    raise "TEST FAILED: expected#{expected.inspect}, received #{received.inspect}"
+    puts "TEST FAILED: expected #{expected.inspect}, received #{received.inspect}"
   end
 end
 
 def factorial_zeros(n)
-
+  two_count, five_count = 0, 0
+  (1..n).each do |i|
+    two_count += count_twos(i)
+    five_count += count_fives(i)
+  end
+  return [two_count, five_count].min
 end
 
 
-assert(factorial_zeros(1) == 0)
-assert(factorial_zeros(10) == 2)
-assert(factorial_zeros(100) == 24)
+assert(factorial_zeros(1), 0)
+assert(factorial_zeros(10), 2)
+assert(factorial_zeros(100), 24)
