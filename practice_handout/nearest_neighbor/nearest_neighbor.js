fcc2e968d5825127bd2e91688b523d97d56433b1
Chris Wright <christopher.wright47@gmail.com>
2014-01-13 16:54:36 -0800

    got neighbors to populate, need to refactor later

--- practice_handout/nearest_neighbor/nearest_neighbor.js
+++ practice_handout/nearest_neighbor/nearest_neighbor.js
@@ -20,23 +20,21 @@ var max = function(x,y) {
 var Person = function(name, position) {
   this.name = name;
   this.position = position;
-  this.neighbors;
+  this.neighbors = [];
 }
-
-
 
 var mergeSort = function(array) {
   if (array.length <= 1) {return array}
   var mid = Math.floor(array.length / 2);
   var firstHalf = mergeSort(array.slice(0, mid));
   var secondHalf = mergeSort(array.slice(mid));
-  return merge(firstHalf, secondHalf);
+  return mergeByPosition(firstHalf, secondHalf);
 }
 
-var merge = function(a, b) {
+var mergeByPosition = function(a, b) {
   var result = [];
   while (a.length > 0 && b.length > 0) {
-    if (a[0] < b[0]) {
+    if (a[0].position < b[0].position) {
       result.push(a.shift());
     }
     else {result.push(b.shift());}
@@ -62,8 +60,40 @@ Array.prototype.eq = function(otherArray) {
   return true;
 }
 
-console.log(mergeSort([1,4,3,2,5,3,1,1,7]).eq([1,1,1,2,3,3,4,5,7]));
+var populateNeighbors = function(peopleArray) {
+  var orderedArray = mergeSort(peopleArray)
+  for (var i in orderedArray) {
+    var position = orderedArray[i].position;
+    orderedArray[i].neighbors = findNeighbors(orderedArray.slice(0), i, position);
+  }
+}
 
+var findNeighbors = function(array, i, position) {
+  array.splice(i, 1);
+  var possibles = array.slice(max(0, i-3), 6);
+  var neighbors = [];
+  while (neighbors.length < 3) {
+    neighbors.push(possibles.splice(nearestNeighborIndex(possibles, position), 1)[0])
+    // console.log('possibles');
+    // console.log(possibles);
+    // console.log('neighbors');
+    // console.log(neighbors);
+  }
+  return neighbors;
+}
+
+var nearestNeighborIndex = function(array, position) {
+  var distance = Math.abs(position - array[0].position);
+  var index = 0;
+  for (var j in array) {
+    var new_distance = Math.abs(array[j].position - position)
+    if (new_distance < distance) {
+      distance = new_distance;
+      index = j;
+    }
+  }
+  return index;
+}
 
 a = new Person('Chris', 3)
 b = new Person('Ross', 4)
@@ -73,3 +103,9 @@ e = new Person('Julie', 7)
 f = new Person('Sam', 8)
 
 people = [a,b,c,d,e,f];
+
+populateNeighbors(people)
+// each element of people now knows its neighbors
+console.log(f.neighbors.eq([e,d,b]))
+console.log(a.neighbors.eq([c,b,e]))
+
