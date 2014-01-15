4c42817d2e4c14b6b75ff12f7a529705feeaa273
Chris Wright <christopher.wright47@gmail.com>
2014-01-14 21:58:50 -0800

    refactored merge sort a little

--- practice_handout/sorting/merge_sort.rb
+++ practice_handout/sorting/merge_sort.rb
@@ -12,9 +12,9 @@ def merge(a,b)
   until a.empty? || b.empty?
     a.first < b.first ? merged_array << a.shift : merged_array << b.shift
   end
-  merged_array << a
-  merged_array << b
-  merged_array.flatten
+  (b.empty? ? a : b).each { |x| merged_array << x }
+  merged_array
+  # merged_array.push(b.empty? ? a : b).flatten
 end
 
 def other_sort(array)
@@ -28,18 +28,30 @@ puts (merge_sort([1,2,3,4]) == [1,2,3,4])
 puts (merge_sort([3,4,2,1]) == [1,2,3,4])
 puts (merge_sort([3,4,2,2,2,1]) == [1,2,2,2,3,4])
 shuffled_array = (1..100).to_a.shuffle
-start = Time.now
-puts (merge_sort(shuffled_array) == (1..100).to_a)
-puts "100 numbers takes #{Time.now - start} with merge sort").to_a.shuffle
-start = Time.now
-puts (merge_sort(long_shuffled) == (1..100000).to_a)
-puts "100000 numbers takes #{Time.now - start} with merge sort"
-
-start = Time.now
-puts (bubble_sort(shuffled_array) == (1..100).to_a)
-puts "100 numbers takes #{Time.now - start} with bubble sort"
+puts merge_sort(shuffled_array) == (1..100).to_a
 long_shuffled = (1..100000).to_a.shuffle
-start = Time.now
-puts (bubble_sort(long_shuffled) == (1..100000).to_a)
-puts "100000 numbers takes #{Time.now - start} with bubble sort"
+puts merge_sort(long_shuffled) == (1..100000).to_a
+
+
+
+
+
+
+
+
+# start = Time.now
+# puts (merge_sort(shuffled_array) == (1..100).to_a)
+# puts "100 numbers takes #{Time.now - start} with merge sort"
+# long_shuffled = (1..100000).to_a.shuffle
+# start = Time.now
+# puts (merge_sort(long_shuffled) == (1..100000).to_a)
+# puts "100000 numbers takes #{Time.now - start} with merge sort"
+
+# start = Time.now
+# puts (other_sort(shuffled_array) == (1..100).to_a)
+# puts "100 numbers takes #{Time.now - start} with bubble sort"
+# long_shuffled = (1..100000).to_a.shuffle
+# start = Time.now
+# puts (other_sort(long_shuffled) == (1..100000).to_a)
+# puts "100000 numbers takes #{Time.now - start} with bubble sort"
 
