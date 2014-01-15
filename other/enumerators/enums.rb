0b84a5f51f00f46620a14719e1d65674e664319b
Chris Wright <christopher.wright47@gmail.com>
2014-01-15 11:33:34 -0800

    refactored enumerator used to generate infinite fiboniccis

--- other/enumerators/enums.rb
+++ other/enumerators/enums.rb
@@ -1,14 +1,38 @@
-fibonacci_number = Enumerator.new do |yielder|
-  i, j = 0, 1
-  loop do
-    yielder.yield j
-    i, j = j, i + j
+class Fibonacci
+  private
+  attr_reader :fibonacci_number
+  def initialize
+    @fibonacci_number = Enumerator.new do |yielder|
+      i, j = 0, 1
+      loop do
+        yielder.yield j
+        i, j = j, i + j
+      end
+    end
   end
+
+  public
+  def next_n_nums(n)
+    # this puts the next n nums,
+    # does not return them
+    n.times {puts fibonacci_number.next}
+  end
+
+  def first_n_nums(n)
+    # returns the first n nums in an array
+    fibonacci_number.first(5)
+  end
+
 end
 
-# this puts the first ten fibonacci numbers
-10.times {puts fibonacci_number.next}
+fibonacci_sequence = Fibonacci.new
 
-# this puts the 11th - 15th fibonacci numbers,
-# b/c the enumerator has already been called ten times
-5.times {puts fibonacci_number.next}
+# this will print the first ten numbers
+fibonacci_sequence.next_n_nums(10)
+
+# and this will print the next ten
+fibonacci_sequence.next_n_nums(10)
+
+# this will p an array of the first 5
+p fibonacci_sequence.first_n_nums(5)
+
