436bd1995aa2c601a588c066972824bb32895b10
Chris Wright <christopher.wright47@gmail.com>
2014-01-20 14:04:26 -0800

    commented in on lambda_proc file

--- other/lambda_vs_proc/lambda_v_proc.rb
+++ other/lambda_vs_proc/lambda_v_proc.rb
@@ -2,7 +2,8 @@
 A lambda is similar to a proc with a couple key differences
 (by my understanding)
   - lambdas will raise errors about the number of arguments passed,
-    while procs will just assign nil values to missing arguments
+    while procs will just assign nil values to missing arguments and
+    ignore extra parameters passed.
   - for return of control, an explicit return in a lambda will just
     return the value to the calling method/routine, while an explicit
     return in a proc will return all the way out of the calling method.
