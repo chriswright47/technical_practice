8814025da14bbde26266e249e1519a2a1054685c
Chris Wright <christopher.wright47@gmail.com>
2014-01-21 16:46:01 -0800

    set up tests for permutations method

--- other/permutations/permutations.rb
+++ other/permutations/permutations.rb
@@ -1,3 +1,10 @@
 def permutations(string)
+  return string if string.length <= 1
+
 
 end
+
+
+puts permutations('a') == 'a'
+puts permutations('ab') == ['ab','ba']
+puts permutations('abc') == ['abc','acb','bac','bca','cab','cba']
