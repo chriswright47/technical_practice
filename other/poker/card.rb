1f5fa63bbf57c9f0d06ae8f1cf0e5e629f139be6
Chris Wright <christopher.wright47@gmail.com>
2014-03-13 10:51:32 -0700

    refactored a little

--- other/poker/card.rb
+++ other/poker/card.rb
@@ -1,15 +1,13 @@
 class Card
 
-  name_to_values = Hash.new {|hash, key| hash[key] = key.to_i}
-  name_to_values['A'] = 14
-  name_to_values['K'] = 13
-  name_to_values['Q'] = 12
-  name_to_values['J'] = 11
-  NAME_TO_VALUES = name_to_values
+  # name_to_values = Hash.new {|hash, key| hash[key] = key.to_i}
+  NAMES_TO_VALUES = { 'A' => 14, 'K' => 13, 'Q' => 12, 'J' => 11 }
 
-  attr_reader :value, :suit
+  attr_reader :value, :suit, :name
   def initialize(name, suit)
-    @value = NAME_TO_VALUES[name]
+    @value = NAMES_TO_VALUES[name] || name.to_i
     @suit = suit
+    @name = name
   end
+
 end
